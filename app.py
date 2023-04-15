1b63a044d2693e329dfc984447c956556c91c79e
Nishith Choudhary <nishithchoudhary@outlook.com>
2023-04-15 21:42:54 +0530

    updated app with initial run

--- app.py
+++ app.py
@@ -1,10 +1,30 @@
 import streamlit as st
 import pickle
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import PassiveAggressiveClassifier
 
 
 st.title('Fake News Detector')
+df = pd.read_csv("news.csv")
 
+labels = df.label
 
-loaded_model = pickle.load(open("model.sav", 'rb'))
+X_train, X_test, y_train, y_test = train_test_split(
+    df['text'], labels, test_size=0.2, stratify=labels, random_state=42)
 
-pred = loaded_model.predict()
+tfidf_vectorizer = TfidfVectorizer(stop_words='english', max_df=0.7)
+
+# Fit and Transform train and test set
+tfidf_train = tfidf_vectorizer.fit_transform(X_train)
+tfidf_test = tfidf_vectorizer.transform(X_test)
+
+# Initialize PassiveAgressiveClassifier
+pac = PassiveAggressiveClassifier(max_iter=50)
+pac.fit(tfidf_train, y_train)
+
+# y_pred = pac.predict(tfidf_test)
+y_pred = pac.predict(tfidf_test[0])
+
+st.write(y_pred)
